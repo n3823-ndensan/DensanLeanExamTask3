8be02d85313ea5e16154d25a5dd8d182da9441cc
NDENSAN\n3823 <n3823@ndensan.co.jp>
2024-10-24 15:18:03 +0900

    Add TodoListGrid and DumyData

---
+++ Components/Pages/Home.razor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Task3.Entities;
+
+namespace Task3.Components.Pages;
+
+public partial class Home
+{
+    private List<ToDoModel> ToDoList = new List<ToDoModel>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadTodoList();
+        StateHasChanged();
+    }
+    public async Task LoadTodoList()
+    {
+        await InitTodoList();
+        StateHasChanged();
+    }
+
+    private async Task InitTodoList()
+    {
+        Console.WriteLine("TodoList読み込み開始");
+        await Task.Delay(3000);
+        ToDoList.Add(
+            new ToDoModel
+            { Title = "Task1", Deadline = DateTime.Now, Status = 0, Content = "Task1 Content"
+            }
+        );
+        ToDoList.Add(
+            new ToDoModel
+            { Title = "Task2", Deadline = DateTime.Now, Status = 1, Content = "Task2 Content"
+            }
+        );
+        ToDoList.Add(
+            new ToDoModel
+            { Title = "Task3", Deadline = DateTime.Now, Status = 2, Content = "Task3 Content"
+            }
+        );
+        Console.WriteLine("TodoList読み込み終了");
+    }
+
+}
---
+++ Entities/ToDoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3.Entities
+{
+    /// <summary> 
+    /// 題名　（必須）文字列
+    /// 期限 （必須）日付
+    /// 状態 （必須）選択入力（0.未着手、1.仕掛中、2.完了、9.無視） 
+    /// 内容 文字列（改行あり） 
+    /// </summary>
+    public class ToDoModel
+    {
+        public required string Title { get; set; }
+        public required DateTime Deadline { get; set; }
+        public required int Status { get; set; }
+        public required string Content { get; set; }
+    }
+}
