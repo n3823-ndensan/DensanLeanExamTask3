ddb3a8bf602171654a0213aca5406c6aabeb271a
NDENSAN\n3823 <n3823@ndensan.co.jp>
2024-10-28 15:36:31 +0900

    Add HomeState and update

--- Components/Pages/Home.razor.cs
+++ Components/Pages/Home.razor.cs
@@ -1,46 +1,22 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Task3.Entities;
+using Task3.Services;
 
 namespace Task3.Components.Pages;
 
 public partial class Home
 {
+    private HomeState? State { get; set; }
 
     private void NavigateToAddTodoPage() => NavigationManager.NavigateTo("/todo/form");
 
-    private List<ToDoModel> ToDoList = new List<ToDoModel>();
-
     protected override async Task OnInitializedAsync()
     {
-        await LoadTodoList();
+        State = new HomeState(ToDoRepository);
+        State.IsLoading = true;
+        await State.LoadTodoList();
+        State.IsLoading = false;
         StateHasChanged();
-    }
-    public async Task LoadTodoList()
-    {
-        //テストのために意図的に3秒待機
-        await Task.Delay(3000);
-
-        ToDoList = await ToDoRepository.GetToDoListAsync();
-        ToDoList.Sort((x, y) => x.CompareTo(y));
-        StateHasChanged();
-    }
-    private string GetContentFirstLine(string? content)
-    {
-        if (string.IsNullOrEmpty(content)) return string.Empty;
-
-        var lines = content.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-        if (lines.Length == 0) return string.Empty;
-
-        if (lines.Length == 1) return lines.First();
-
-        return $"{lines.First()} ...";
-
-    }
-
-    private string GetStatusName(int no)
-    {
-        return ToDoModel.Statuses.FirstOrDefault(s => s.No == no).Name;
     }
 
     private void OnUpdateRow(ToDoModel todo)
---
+++ Services/HomeState.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Task3.Repository.Todo;
+using Task3.Entities;
+
+namespace Task3.Services;
+
+
+public class HomeState
+{
+    private IToDoRepository toDoRepository { get; }
+
+    public HomeState(IToDoRepository toDoRepository)
+    {
+        //nullチェックと設定
+        this.toDoRepository = toDoRepository ?? throw new ArgumentNullException(nameof(toDoRepository));
+    }
+
+    public bool IsLoading { get; set; } = true;
+
+    public List<ToDoModel> ToDoList { get; set; } = new List<ToDoModel>();
+
+    public async Task LoadTodoList()
+    {
+        //テストのために意図的に3秒待機
+        await Task.Delay(3000);
+
+        ToDoList = await toDoRepository.GetToDoListAsync();
+        ToDoList.Sort((x, y) => x.CompareTo(y));
+    }
+
+    public string GetContentFirstLine(string? content)
+    {
+        if (string.IsNullOrEmpty(content)) return string.Empty;
+
+        var lines = content.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (lines.Length == 0) return string.Empty;
+
+        if (lines.Length == 1) return lines.First();
+
+        return $"{lines.First()} ...";
+
+    }
+
+    public string GetStatusName(int no)
+    {
+        return ToDoModel.Statuses.FirstOrDefault(s => s.No == no).Name;
+    }
+}
